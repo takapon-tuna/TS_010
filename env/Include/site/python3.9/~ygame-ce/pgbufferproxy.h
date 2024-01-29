8cdcbc42505cb5d80acb19ec9a8dae41e8d7da33
takapon <nakamura.takaharu@icloud.comgit config user.namegit config --global user.email nakamura.takaharu@icloud.com>
2024-01-29 15:45:47 +0900

    fix : envを削除
    今回で分かったけどenvは全く必要ない

--- env/Include/site/python3.9/~ygame-ce/pgbufferproxy.h
+++
@@ -1,7 +0,0 @@
-#ifndef PG_BUFPROXY_INTERNAL_H
-#define PG_BUFPROXY_INTERNAL_H
-
-#include "include/pygame_bufferproxy.h"
-#define PYGAMEAPI_BUFPROXY_NUMSLOTS 4
-
-#endif /* ‾PG_BUFPROXY_INTERNAL_H */
